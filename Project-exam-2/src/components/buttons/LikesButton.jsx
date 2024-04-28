27da18360b28a617a3c1107e5fab76d26918d370
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-04-28 23:53:52 +0200

    Updated my files

--- Project-exam-2/src/components/buttons/LikesButton.jsx
+++ Project-exam-2/src/components/buttons/LikesButton.jsx
@@ -1,5 +1,5 @@
-
 import { StyledLikesButton } from "./Button.styles";
+
 
 function LikesButton() {
 
--- Project-exam-2/src/components/images/BackgroundImage.jsx
+++ Project-exam-2/src/components/images/BackgroundImage.jsx
@@ -2,6 +2,7 @@ import Background from "../../images/modern3.jpg";
 import { StyledBackgroundImage } from "./BackgroundImage.styles";
 
 function BackgroundImage() {
+    
     return <StyledBackgroundImage src={Background}  alt="Background Image" />
 }
 
